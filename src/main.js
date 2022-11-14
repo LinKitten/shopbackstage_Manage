49e2ed1eed657c4fa38c8b9367faa3ed0e73982c
LinKitten <2634024527@qq.com>
2022-11-14 21:15:41 +0800

    login

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import SvgIcon from '@/icons'
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import '././assets/styles/border.css'
+import '././assets/styles/reset.css'
 
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).use(SvgIcon).mount('#app')

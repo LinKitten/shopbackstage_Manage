514ba4ee375a89f50f922cc6c3303eaf93d34f31
LinKitten <2634024527@qq.com>
2022-11-20 23:20:04 +0800

    添加商品大类管理

---
+++ src/views/bigType/components/imageDialog.vue
@@ -0,0 +1,131 @@
+<template>
+  <el-dialog
+    model-value="imageDialogVisible"
+    title="商品大类图片更换"
+    width="40%"
+    @close="handleClose"
+    center
+  >
+    <el-form ref="formRef" :model="form" label-width="100px" style="text-align:center;">
+      <el-upload
+      :headers="headers"
+        class="avatar-uploader"
+        :action="getServerUrl()+'admin/bigType/uploadImage'"
+        :show-file-list="false"
+        :on-success="handleAvatarSuccess"
+        :before-upload="beforeAvatarUpload"
+      >
+        <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+        <el-icon v-else class="avatar-uploader-icon">
+          <Plus />
+        </el-icon>
+      </el-upload>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button type="primary" @click="handleConfirm"
+          >确认更换图片</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import axios,{ getServerUrl } from "@/util/axios";
+import { defineEmits, defineProps, watch, ref } from "vue";
+import { ElMessage } from "element-plus";
+import { Plus } from '@element-plus/icons-vue'
+
+
+const tableDate = ref([]);
+
+//接收父组件传过来的id
+const prop = defineProps({
+  imageDialogValue:{
+    type: Object,
+    default: ()=>{},
+    requirted: true,
+  }
+});
+
+//获取token
+const headers = ref({
+  token:window.sessionStorage.getItem("token")
+})
+
+
+const form = ref({
+  id: -1,
+  image: "",
+
+});
+
+const formRef = ref(null);
+const imageUrl = ref('')
+// 监听id的变化
+watch(
+  () => prop.imageDialogValue, //监听的参数
+  //监听的回调方法
+  () => {
+   form.value = prop.imageDialogValue
+  //  让图片回显
+  imageUrl.value = getServerUrl()+"/image/bigType/"+form.value.image
+  },
+  {deep:true,immediate:true}
+);
+
+const emits = defineEmits(["update:modelValue", "initBigTypeList"]);
+
+const handleClose = () => {
+  emits("update:modelValue", false);
+};
+
+const handleConfirm = () => {
+  formRef.value.validate(async (valid) => {
+    if (valid) {
+      let result = await axios.post("admin/bigType/save", form.value);
+      let data = result.data;
+      if (data.code == 0) {
+        ElMessage.success("执行成功");
+        formRef.value.resetFields();
+        emits("initBigTypeList"); //调用父组件的方法 更新页面
+        handleClose();
+      } else {
+        ElMessage.error(data.msg);
+      }
+    } else {
+      console.log("fail");
+      return false;
+    }
+  });
+};
+</script>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
+
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
--- src/views/bigType/index.vue
+++ src/views/bigType/index.vue
@@ -1,11 +1,137 @@
 <template>
-  商品大类
+  <el-card class="box-card">
+    <!-- 用户搜索框 -->
+    <el-row :gutter="20" class="header">
+      <el-col :span="7">
+        <el-input placeholder="请输入商品大类名称......" v-model="queryForm.query" clearable></el-input>
+      </el-col>
+      <el-button type="primary" :icon="Search" @click="initBigTypeList">搜索</el-button>
+      <el-button type="primary" :icon="DocumentAdd" @click="handleDialogValue()">添加商品大类</el-button>
+    </el-row>
+
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="id" label="#ID" width="80" />
+      <el-table-column prop="name" label="商品大类名称" width="200" />
+      <el-table-column prop="image" label="商品大类图片" width="200">
+        <template v-slot="scope">
+          <img :src="getServerUrl() + '/image/bigType/' + scope.row.image" width="80" height="80" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="remark" label="商品大类描述" />
+      <el-table-column prop="action" label="操作" width="300" fixed="right">
+        <template v-slot="scope">
+          <el-button type="success" @click="handleImageDialogValue(scope.row)">更换商品图片</el-button>
+          <el-button type="primary" :icon="Edit" @click="handleDialogValue(scope.row.id)">
+          </el-button>
+          <el-button type="danger" :icon="Delete" @click="handleDelete(scope.row.id)">
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination v-model:current-page="queryForm.pageNum" v-model:page-size="queryForm.pageSize"
+      :page-sizes="[10, 20, 30, 40, 50]" layout="total, sizes, prev, pager, next, jumper" :total="total"
+      @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+  </el-card>
+  <Dialog v-model="dialogVisible" :id="id" :dialogTitle="dialogTitle" @initBigTypeList="initBigTypeList"></Dialog>
+  <ImageDialog v-model="imageDialogVisible" :imageDialogValue="imageDialogValue" @initBigTypeList="initBigTypeList">
+  </ImageDialog>
 </template>
 
 <script setup>
+import { Search, Delete, Edit, DocumentAdd } from "@element-plus/icons-vue";
+import { ref } from "vue";
+import axios, { getServerUrl } from "@/util/axios";
+import Dialog from "./components/dialog.vue";
+import ImageDialog from "./components/imageDialog.vue";
+import { ElMessage, ElMessageBox } from "element-plus";
 
+const queryForm = ref({
+  query: "",
+  pageNum: 1, //当前页
+  pageSize: 10, //每页大小
+});
+const total = ref(0); //默认总记录
+
+const tableData = ref([]);
+
+const id = ref(-1);
+
+const dialogTitle = ref("");
+
+const dialogVisible = ref(false);
+
+const imageDialogValue = ref({});
+
+const imageDialogVisible = ref(false);
+
+const initBigTypeList = async () => {
+  const res = await axios.post("admin/bigType/list", queryForm.value);
+  tableData.value = res.data.bigTypeList;
+  total.value = res.data.total;
+};
+initBigTypeList();
+
+const handleSizeChange = (pageSize) => {
+  queryForm.value.pageNum = 1;
+  queryForm.value.pageSize = pageSize;
+  initBigTypeList();
+};
+const handleCurrentChange = (pageNum) => {
+  queryForm.value.pageNum = pageNum;
+  initBigTypeList();
+};
+
+const handleDialogValue = (bigTypeId) => {
+  if (bigTypeId) {
+    //如果有id，则是修改
+    id.value = bigTypeId;
+    dialogTitle.value = "商品大类修改";
+  } else {
+    id.value = -1;
+    dialogTitle.value = "商品大类添加";
+  }
+  dialogVisible.value = true; //控制dialog是否显示
+};
+
+//更改图片
+const handleImageDialogValue = (row) => {
+  imageDialogVisible.value = true;
+  imageDialogValue.value = JSON.parse(JSON.stringify(row));
+};
+
+const handleDelete = (id) => {
+  ElMessageBox.confirm("确定要删除这条记录吗？", "系统提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning",
+  })
+    .then(async () => {
+      let res = await axios.get("admin/bigType/delete/" + id);
+      if (res.data.code == 0) {
+        ElMessage({
+          type: "success",
+          message: "删除成功",
+        });
+        initBigTypeList(); //刷新
+      } else {
+        ElMessage({
+          type: "error",
+          message: res.data.msg,
+        });
+      }
+    })
+    .catch(() => { });
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.header {
+  padding-bottom: 16px;
+  box-sizing: border-box;
+}
 
+.el-pagination {
+  padding-top: 15px;
+  box-sizing: border-box;
+}
 </style>
